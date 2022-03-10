0624d5a79fa74e7557d05578e780f09dc384c098
Remigijus <achmudass@gmail.com>
2022-03-10 12:28:53 +0200

    About me moving text to variables

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  title = "Some things about me";
+
+  aboutMeGeneralContent = 
+  `My name is Remigijus. I'm from Lithuania, a small country in Europe near Baltic sea.
+  
+  I am always trying to learn new technologies that I would be able to use them in job-related and my personal
+  projects. That's why I'm always working on some projects to learn those technologies as in my opinion the best way
+  to learn something is just by doing.
+
+  Another goal of mine is to create passive income by creating things which would be useful.`;
+
+  aboutMeGamesContent = 
+  `I'm passionate about video games. I have created some game prototypes using Unity. You can find them in Portfolio page.`;
+  
+  aboutMePersonalContent = 
+  `To relax from all the computer stuff I'm reading books and keeping the bees.`;
+
+  githubLink = 'https://github.com/achmudas';
+  linkedInLink = 'https://www.linkedin.com/in/remigijuskutkaitis';
+  twitterLink = 'https://twitter.com/achmudas';
+
   constructor() { }
 
   ngOnInit(): void {
